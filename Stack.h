f5f9542988086fb9748e2a197248388aec92da6e
Daniel Heater <daniel@heatertech.com>
2020-03-31 23:15:52 -0500

    Tests passing

---
+++ Stack.h
@@ -0,0 +1,70 @@
+#pragma once
+
+namespace
+{
+    struct Node {
+        Node *next = nullptr;
+        Node *prev = nullptr;
+        int value{};
+    };
+} // namespace
+
+class Stack
+{
+    Node *head = nullptr;
+
+  public:
+    void Push(int value)
+    {
+        Node *n = new Node;
+        n->value = value;
+
+        if(head) { n->next = head; }
+        head = n;
+    }
+
+    int Pop()
+    {
+        if(!head) { throw 0; }
+
+        Node *n = head;
+        head = n->next;
+        int value = n->value;
+        delete n;
+        return value;
+    }
+};
+
+class Queue
+{
+    Node *head = nullptr;
+    Node *tail = nullptr;
+
+  public:
+    void Enqueue(int value)
+    {
+        Node *n = new Node;
+        n->value = value;
+        n->next = head;
+
+        if(head) {
+            head->prev = n;
+        } else {
+            tail = n;
+        }
+        head = n;
+    }
+
+    int Dequeue()
+    {
+        if(!tail) {
+            throw 0;
+        }
+        Node *n = tail;
+        int rval = n->value;
+        tail = n->prev;
+
+        delete n;
+        return rval;
+    }
+};
--- test.cpp
+++ test.cpp
@@ -1,6 +1,43 @@
+#include "Stack.h"
 #include <catch2/catch.hpp>
 
-TEST_CASE("test")
+TEST_CASE("Test stack operations")
 {
-    CHECK(false);
+    Stack stack{};
+
+    stack.Push(1);
+    stack.Push(2);
+    CHECK(2 == stack.Pop());
+    CHECK(1 == stack.Pop());
+
+    // Try to pop from an empty stack
+    CHECK_THROWS(stack.Pop());
 }
+
+TEST_CASE("Test pop from just created stack throws")
+{
+    Stack stack;
+
+    CHECK_THROWS(stack.Pop());
+}
+
+TEST_CASE("Test queue operations")
+{
+    Queue queue;
+
+    queue.Enqueue(1);
+    queue.Enqueue(2);
+    CHECK(1 == queue.Dequeue());
+    CHECK(2 == queue.Dequeue());
+
+    // Try to dequeue from an empty queue
+    CHECK_THROWS(3 == queue.Dequeue());
+}
+
+TEST_CASE("Test dequeu from just created queue throws")
+{
+    Queue queue;
+
+    CHECK_THROWS(queue.Dequeue());
+}
+
